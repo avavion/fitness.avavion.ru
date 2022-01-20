88bf3f0f151b29cf939973012a96b738872bacbf
avavion <avavionmvm@gmail.com>
2022-01-21 01:48:19 +0300

    Added: review section and slider for reviews

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,14 +39,51 @@ const header = {
 }
 
 const reviews = {
-    config: {
-
+    selectors: {
+        slider: '.js-slider',
+        el: '.reviews'
     },
-    slider: (selector) => {
-        const elements = document.querySelectorAll(selector);
+    config(options = {}) {
+        const settings = {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            loop: true,
+            speed: 500,
+            autoplay: {
+                delay: 5000
+            },
+            spaceBetween: 120,
+            direction: 'horizontal',
+            effect: 'coverflow',
+            coverflowEffect: {
+                rotate: 0,
+                modifer: 1,
+                slideShadows: false,
+                stretch: 0
+            },
+        };
+
+        return Object.assign(settings, options);
+    },
+    slider(element, options) {
+        return new Swiper(element, this.config(options));
+    },
+    init(...options) {
+        const elements = document.querySelectorAll(`${this.selectors.el} ${this.selectors.slider}`);
 
         for (const element of elements) {
-            const slider = new Swiper(element, reviews.config);
+            const slider = this.slider(element, options);
+
+            slider.slides[slider.activeIndex + 1].classList.add('_active');
+
+            slider.on('slideChange', (swiper) => {
+                for (const slide of swiper.slides) {
+                    slide.classList.remove('_active');
+                }
+
+                swiper.slides[swiper.activeIndex + 1].classList.add('_active');
+            });
+
         }
     }
 };
@@ -55,6 +92,7 @@ const reviews = {
 const init = () => {
     dd('init();');
     header.init();
+    reviews.init();
 }
 
 document.addEventListener('DOMContentLoaded', init);
